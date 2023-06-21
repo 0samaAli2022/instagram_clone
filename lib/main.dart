ab9995aff418e27359ac0d98b18acb9967c792c5
osama <osamaali31121999@gmail.com>
2023-06-22 00:55:12 +0300

    adding a loading screen using Progress HUD package

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
 import 'package:instagram_clone/state/auth/providers/is_logged_in_provider.dart';
+import 'package:instagram_clone/views/components/constants/strings.dart';
 import 'firebase_options.dart';
 
 import 'dart:developer' as devtools show log;
@@ -51,15 +53,24 @@ class MainView extends StatelessWidget {
         title: const Text('Main View'),
         centerTitle: true,
       ),
-      body: Consumer(
-        builder: (context, ref, child) {
-          return TextButton(
-            onPressed: () async {
-              await ref.read(authStateProvider.notifier).logOut();
-            },
-            child: const Text('Log Out!'),
-          );
-        },
+      body: ProgressHUD(
+        backgroundColor: Colors.white,
+        indicatorColor: Colors.blueGrey,
+        textStyle: const TextStyle(color: Colors.blueGrey, fontSize: 15),
+        borderColor: Colors.black,
+        child: Consumer(
+          builder: (context, ref, child) {
+            return TextButton(
+              onPressed: () async {
+                final progress = ProgressHUD.of(context)!;
+                progress.showWithText(Strings.loading);
+                await ref.read(authStateProvider.notifier).logOut();
+                progress.dismiss();
+              },
+              child: const Text('Log Out!'),
+            );
+          },
+        ),
       ),
     );
   }
@@ -75,18 +86,38 @@ class LoginView extends ConsumerWidget {
         title: const Text('Login View'),
         centerTitle: true,
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextButton(
-            onPressed: ref.read(authStateProvider.notifier).loginWithGoogle,
-            child: const Text('Sign In with Google'),
-          ),
-          TextButton(
-            onPressed: ref.read(authStateProvider.notifier).loginWithFacebook,
-            child: const Text('Sign In with Facebook'),
-          ),
-        ],
+      body: ProgressHUD(
+        backgroundColor: Colors.white,
+        indicatorColor: Colors.blueGrey,
+        textStyle: const TextStyle(color: Colors.blueGrey, fontSize: 14),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Builder(builder: (context) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  final progress = ProgressHUD.of(context);
+                  progress?.showWithText(Strings.loading);
+                  await ref.read(authStateProvider.notifier).loginWithGoogle();
+                  progress?.dismiss();
+                },
+                child: const Text('Sign In with Google'),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final progress = ProgressHUD.of(context);
+                  progress?.showWithText(Strings.loading);
+                  await ref
+                      .read(authStateProvider.notifier)
+                      .loginWithFacebook();
+                  progress?.dismiss();
+                },
+                child: const Text('Sign In with Facebook'),
+              ),
+            ],
+          );
+        }),
       ),
     );
   }
