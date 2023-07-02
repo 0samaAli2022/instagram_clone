31e1a5390975ab90e372d0f916b310f6a44ef16c
osama <osamaali31121999@gmail.com>
2023-07-02 03:18:16 +0300

    login view completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: use_build_context_synchronously
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_progress_hud/flutter_progress_hud.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone/state/auth/providers/auth_state_provider.dart';
 import 'package:instagram_clone/state/auth/providers/is_logged_in_provider.dart';
+import 'package:instagram_clone/utils/loading_dialog.dart';
 import 'package:instagram_clone/views/components/constants/strings.dart';
+import 'package:instagram_clone/views/login/login_view.dart';
 import 'firebase_options.dart';
 
 import 'dart:developer' as devtools show log;
@@ -35,7 +37,7 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Consumer(
         builder: (context, ref, child) {
-          final isLoggedIn = ref.watch(isLoggedInProvider);
+          final isLoggedIn = ref.watch(isLoggedInProvider); 
           return isLoggedIn ? const MainView() : const LoginView();
         },
       ),
@@ -53,71 +55,17 @@ class MainView extends StatelessWidget {
         title: const Text('Main View'),
         centerTitle: true,
       ),
-      body: ProgressHUD(
-        backgroundColor: Colors.white,
-        indicatorColor: Colors.blueGrey,
-        textStyle: const TextStyle(color: Colors.blueGrey, fontSize: 15),
-        borderColor: Colors.black,
-        child: Consumer(
-          builder: (context, ref, child) {
-            return TextButton(
-              onPressed: () async {
-                final progress = ProgressHUD.of(context)!;
-                progress.showWithText(Strings.loading);
-                await ref.read(authStateProvider.notifier).logOut();
-                progress.dismiss();
-              },
-              child: const Text('Log Out!'),
-            );
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class LoginView extends ConsumerWidget {
-  const LoginView({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login View'),
-        centerTitle: true,
-      ),
-      body: ProgressHUD(
-        backgroundColor: Colors.white,
-        indicatorColor: Colors.blueGrey,
-        textStyle: const TextStyle(color: Colors.blueGrey, fontSize: 14),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-        child: Builder(builder: (context) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextButton(
-                onPressed: () async {
-                  final progress = ProgressHUD.of(context);
-                  progress?.showWithText(Strings.loading);
-                  await ref.read(authStateProvider.notifier).loginWithGoogle();
-                  progress?.dismiss();
-                },
-                child: const Text('Sign In with Google'),
-              ),
-              TextButton(
-                onPressed: () async {
-                  final progress = ProgressHUD.of(context);
-                  progress?.showWithText(Strings.loading);
-                  await ref
-                      .read(authStateProvider.notifier)
-                      .loginWithFacebook();
-                  progress?.dismiss();
-                },
-                child: const Text('Sign In with Facebook'),
-              ),
-            ],
+      body: Consumer(
+        builder: (context, ref, child) {
+          return TextButton(
+            onPressed: () async {
+              loadingScrean(context, Strings.loading);
+              await ref.read(authStateProvider.notifier).logOut();
+              Navigator.of(context).pop();
+            },
+            child: const Text('Log Out!'),
           );
-        }),
+        },
       ),
     );
   }
